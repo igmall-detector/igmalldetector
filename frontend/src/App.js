ba78f32f22dcd77ea969c6ba65e0ca70c52af385
igmall-detector <dorsynaya@gmail.com>
2025-07-06 14:02:44 -0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to IG Mall Detector</h1>
+      <p>Scan and avoid scam Instagram sellers.</p>
+    </div>
+  );
+}
+
+export default App;
